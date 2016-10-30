d1e0977e17a1dda3282655643a31305fe2f3dda7
Elizabeth Sparks <erhsparks@gmail.com>
2016-10-29 23:47:02 -0700

    fix typo in webpack config; add a React DOM listener

---
+++ frontend/huluballoo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(
+    <h1>
+      Welcome to Huluballoo!
+    </h1>,
+    root
+  );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/huluballo.jsx',
+  entry: path.join(__dirname, 'frontend', 'huluballoo.jsx'),
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
