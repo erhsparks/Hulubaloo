c5346fd4785c881d49c01b97a0de70a65c8b2cc9
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-02 09:25:32 -0700

    refactor for cleanliness

--- frontend/components/header/header_login_links.jsx
+++ frontend/components/header/header_login_links.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 import LoggedIn from './logged_in';
 import LoggedOut from './logged_out';
 
-export default ({ currentUser, logout }) => {
-  if (currentUser) {
-    return (
-      <LoggedOut currentUser={currentUser} logout={logout}/>
-    );
-  } else {
-    return (
-      <LoggedIn />
-    );
-  }
-};
+export default ({ currentUser, logout }) => (
+  currentUser ? <LoggedOut currentUser={currentUser} logout={logout}/> : <LoggedIn />
+);
