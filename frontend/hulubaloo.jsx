5c76ee8150c63134cbd631a02582717eaed2ca4c
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-02 10:34:51 -0700

    add preliminary header toggle: now to clean it up!

--- frontend/hulubaloo.jsx
+++ frontend/hulubaloo.jsx
@@ -17,4 +17,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
 
   ReactDOM.render(<Root store={store} />, root);
+
+  const toggleHeaderBkg = () => {
+    let header = document.getElementsByTagName('header')[0];
+    let mastheadBottom = 307;
+
+    let onMasthead = 'on-masthead';
+    let offMasthead = 'off-masthead';
+
+    let onClass;
+    let offClass;
+
+    if (document.body.scrollTop <= mastheadBottom) {
+      onClass = onMasthead;
+      offClass = offMasthead;
+    } else {
+      onClass = offMasthead;
+      offClass = onMasthead;
+    }
+
+    header.classList.add(onClass);
+    header.classList.remove(offClass);
+  };
+
+  window.onscroll = () => toggleHeaderBkg();
 });
