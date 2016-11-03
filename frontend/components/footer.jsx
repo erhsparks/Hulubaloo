68be5f3cd5dc838da3c0febe6b123da7fee624f2
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-03 13:50:57 -0700

    add links to a few of the footer a tags

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,10 +5,10 @@ export default () => (
     <nav className='footer-nav page-width'>
       <ul className='footer-ul'>
         <li><a>About</a></li>
-        <li><a>Contact</a></li>
+        <li><a href='mailto:erhsparks@gmail.com&amp;subject=Hulubaloo&amp;body=I%20found%20your%20Hulubaloo%20site%20and...'>Contact</a></li>
         <li><a>Jobs</a></li>
-        <li><a>Github</a></li>
-        <li><a>Source code</a></li>
+        <li><a href='https://github.com/erhsparks'>Github</a></li>
+        <li><a href='https://github.com/erhsparks/Hulubaloo'>Source code</a></li>
       </ul>
 
       <ul className='footer-ul'>
