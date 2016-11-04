6cbd512639936cab4c6854744b7c49fc2d9f486b
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-03 18:00:36 -0700

    links open in new tab so that visitors don't forget about my page

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -4,16 +4,16 @@ export default () => (
   <footer>
     <nav className='footer-nav page-width'>
       <ul className='footer-ul'>
-        <li><a>About</a></li>
-        <li><a href='mailto:erhsparks@gmail.com?&amp;subject=Hulubaloo&amp;body=I%20found%20your%20Hulubaloo%20site%20and...'>Contact</a></li>
-        <li><a>Jobs</a></li>
-        <li><a href='https://github.com/erhsparks'>Github</a></li>
-        <li><a href='https://github.com/erhsparks/Hulubaloo'>Source code</a></li>
+        <li><a target="_blank">About</a></li>
+        <li><a target="_blank" href='mailto:erhsparks@gmail.com?&amp;subject=Hulubaloo&amp;body=I%20found%20your%20Hulubaloo%20site%20and...'>Contact</a></li>
+        <li><a target="_blank">Jobs</a></li>
+        <li><a target="_blank" href='https://github.com/erhsparks'>Github</a></li>
+        <li><a target="_blank" href='https://github.com/erhsparks/Hulubaloo'>Source code</a></li>
       </ul>
 
       <ul className='footer-ul'>
-        <li><a>Terms of Use</a></li>
-        <li><a>Privacy Policy</a></li>
+        <li><a target="_blank">Terms of Use</a></li>
+        <li><a target="_blank">Privacy Policy</a></li>
         <li>© 2016 Elizabeth Sparks</li>
       </ul>
     </nav>
