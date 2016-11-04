0ad9c6be9f3397cc1b397b5ebbb32181e70e9bf9
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-03 17:58:02 -0700

    fix broken mailto link

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,7 +5,7 @@ export default () => (
     <nav className='footer-nav page-width'>
       <ul className='footer-ul'>
         <li><a>About</a></li>
-        <li><a href='mailto:erhsparks@gmail.com&amp;subject=Hulubaloo&amp;body=I%20found%20your%20Hulubaloo%20site%20and...'>Contact</a></li>
+        <li><a href='mailto:erhsparks@gmail.com?&amp;subject=Hulubaloo&amp;body=I%20found%20your%20Hulubaloo%20site%20and...'>Contact</a></li>
         <li><a>Jobs</a></li>
         <li><a href='https://github.com/erhsparks'>Github</a></li>
         <li><a href='https://github.com/erhsparks/Hulubaloo'>Source code</a></li>
