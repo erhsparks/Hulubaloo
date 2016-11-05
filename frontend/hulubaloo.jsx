de93f0cf0719e633e1138eb744d7bfb81597b6c2
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-04 17:09:24 -0700

    begin front page featured categories components

--- frontend/hulubaloo.jsx
+++ frontend/hulubaloo.jsx
@@ -6,28 +6,15 @@ import Modal from 'react-modal';
 import Root from './components/root';
 import toggleHeaderBkg from './header_scroll_handler';
 
-import {
-  fetchAllVideos,
-  fetchVideo,
-  fetchAllCategories,
-  fetchVideosInCategory
- } from './actions/video_actions';
-
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = checkForLogin();
   const store = configureStore(preloadedState);
 
   Modal.setAppElement(document.body);
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 
   toggleHeaderBkg();
   window.onscroll = () => toggleHeaderBkg();
-
-// this commit only; for testing
-  window.fetchAllVideos = fetchAllVideos;
-  window.fetchVideo = fetchVideo;
-  window.fetchAllCategories = fetchAllCategories;
-  window.fetchVideosInCategory = fetchVideosInCategory;
-  window.store = store;
 });
