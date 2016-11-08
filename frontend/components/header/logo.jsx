528013a8cace45de732667587373215032a9bd68
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-07 16:21:45 -0800

    move logo size from inline to stylesheet

--- frontend/components/header/logo.jsx
+++ frontend/components/header/logo.jsx
@@ -4,7 +4,6 @@ export default () => (
   <a href='/'>
     <img className='hulubaloo-logo'
     src='/assets/logo.png'
-    alt='hulubaloo logo'
-    style={{width: 120, height: 20}} />
+    alt='hulubaloo logo' />
   </a>
 );
