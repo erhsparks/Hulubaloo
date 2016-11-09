5a9b3770814ef4918cd2baded3acd262f88557cb
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-08 17:00:01 -0800

    add play button to video images on hover to make it more obvious that they're links to that video

--- frontend/components/categories/category_video.jsx
+++ frontend/components/categories/category_video.jsx
@@ -4,21 +4,22 @@ import { Link } from 'react-router';
 const CategoryVideo = ({ video }) => {
   if (video.title === 'Placeholder') {
     return (
-      <li>
-        <img className='featured-video no-show'
+      <li className='featured-video no-show'>
+        <img
           src='/assets/transparent_1px.gif'
           alt='' />
       </li>
     );
   } else {
     return (
-      <li>
-        <Link to={`watch/${video.id}`}>
-          <img className='featured-video'
+      <Link to={`watch/${video.id}`}>
+        <li className='featured-video'>
+          <img
             src={video.preview_image_url}
             alt={`Poster link to ${video.title}`} />
-        </Link>
-      </li>
+          <div className='play-button' />
+        </li>
+      </Link>
     );
   }
 };
