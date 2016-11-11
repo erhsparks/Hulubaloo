14222d0b8b6d40b450eda8357b4a45aea48541aa
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-11 04:33:18 -0800

    add categories to header and movie nights to user dropdown; fix style bugs

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Logo from './logo';
+import { Link } from 'react-router';
 import HeaderLoginLinksContainer from './header_login_links_container';
 
 export default () => (
   <header className='global-header'>
     <nav className='header-nav page-width'>
-      <Logo />
+      <ul className='user-menu'>
+        <li><Logo /></li>
+        <li><Link to='categories'>Categories</Link></li>
+      </ul>
       <HeaderLoginLinksContainer />
     </nav>
   </header>
--- frontend/components/header/logged_in.jsx
+++ frontend/components/header/logged_in.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default ({ currentUser, logout }) => (
   <ul className='user-menu'>
     <li className='user-dropdown'>
-      <a>{currentUser.username.toUpperCase()}</a>
+      <a>{currentUser.username}</a>
 
-      <ul className='user-dropdown-items'>
-        <li><a onClick={() => logout()}>Log Out</a></li>
-      </ul>
+      <div>
+        <ul className='user-dropdown-items'>
+          <li><Link to='movie-nights'>My Movie Nights</Link></li>
+          <li><a onClick={() => logout()}>Log Out</a></li>
+        </ul>
+      </div>
     </li>
   </ul>
 );
