7b5e026644b7bb2594bea54c5e3a9ada7cbbc3da
Elizabeth Sparks <erhsparks@gmail.com>
2016-12-08 21:02:19 -0800

    make hulubaloo logo a tag bkg image for faster loading

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Logo from './logo';
 import HeaderLoginLinksContainer from './header_login_links_container';
 
 export default () => (
   <header className='global-header'>
     <nav className='header-nav page-width'>
       <ul className='user-menu'>
-        <li><Logo /></li>
+        <li><a href='/' className='hulubaloo-logo'></a></li>
         <li><a href='/#/categories'>Categories</a></li>
       </ul>
       <HeaderLoginLinksContainer />
--- frontend/components/header/logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => (
-  <a href='/'>
-    <img className='hulubaloo-logo'
-    src='/assets/logo.png'
-    alt='hulubaloo logo' />
-  </a>
-);
