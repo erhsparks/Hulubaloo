2904c2581eabe7f9bba2c3578a3e0d2676084f6c
Elizabeth Sparks <erhsparks@gmail.com>
2016-12-08 20:55:32 -0800

    change link to a tag to remove hashHistory warning

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Logo from './logo';
-import { Link } from 'react-router';
 import HeaderLoginLinksContainer from './header_login_links_container';
 
 export default () => (
@@ -8,7 +7,7 @@ export default () => (
     <nav className='header-nav page-width'>
       <ul className='user-menu'>
         <li><Logo /></li>
-        <li><Link to='categories'>Categories</Link></li>
+        <li><a href='/#/categories'>Categories</a></li>
       </ul>
       <HeaderLoginLinksContainer />
     </nav>
