bef898af92b31d8841208a3c11ba548a94b89c48
Elizabeth Sparks <erhsparks@gmail.com>
2016-12-14 11:07:53 -0800

    factor out maskhead bkg specifications to their own file

--- frontend/components/masthead/masthead.jsx
+++ frontend/components/masthead/masthead.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MastheadFeatured from './masthead_background';
 import SignupButtons from './signup_buttons';
 
 const Masthead = ({ currentUser }) => {
@@ -12,20 +13,7 @@ const Masthead = ({ currentUser }) => {
 
   return (
     <section className='masthead'>
-      <div className='masthead-featured'>
-        <img
-          src='https://res.cloudinary.com/erhsparks/image/upload/v1478295419/max_cxnhhu.jpg'
-          alt='Mad Max: Fury Road poster' />
-        <img
-          src='https://res.cloudinary.com/erhsparks/image/upload/v1478295419/thelma_louise_pubmpj.jpg'
-          alt='Thelma and Louise poster' />
-        <img
-          src='https://res.cloudinary.com/erhsparks/image/upload/v1478295418/clueless_bfehko.jpg'
-          alt='Clueless poster' />
-        <img
-          src='https://res.cloudinary.com/erhsparks/image/upload/v1478295418/iron_giant_nr8gqb.jpg'
-          alt='The Iron Giant poster' />
-      </div>
+      <MastheadFeatured />
 
       <article className='masthead-text'>
         <h1>
---
+++ frontend/components/masthead/masthead_background.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MastheadFeatured = () => (
+  <div className='masthead-featured'>
+    <img
+      src='https://res.cloudinary.com/erhsparks/image/upload/v1478295419/max_cxnhhu.jpg'
+      alt='Mad Max: Fury Road poster' />
+    <img
+      src='https://res.cloudinary.com/erhsparks/image/upload/v1478295419/thelma_louise_pubmpj.jpg'
+      alt='Thelma and Louise poster' />
+    <img
+      src='https://res.cloudinary.com/erhsparks/image/upload/v1478295418/clueless_bfehko.jpg'
+      alt='Clueless poster' />
+    <img
+      src='https://res.cloudinary.com/erhsparks/image/upload/v1478295418/iron_giant_nr8gqb.jpg'
+      alt='The Iron Giant poster' />
+  </div>
+);
+
+export default MastheadFeatured;
